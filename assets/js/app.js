aeae207e2e35fb1283fb4c5017fd7501283bd55a
vandeefeng <naoyanzifzm@hotmail.com>
2024-10-25 00:08:27 +0800

    add lazyload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,20 @@
 $(document).ready(function () {
-  // 为非封面图片添加 data-fancybox="gallery" 属性
-  $('.img').not(".cover").each(function () {
-    $(this).attr("data-fancybox", "gallery");
+  // 为非封面图片添加 data-fancybox="gallery" 属性并替换 src 为 data-src
+  $('.lazy').not(".cover").each(function () {
+    // 获取当前图片的 src
+    const originalSrc = $(this).attr("src");
+    
+    // 设置 data-src 和 data-fancybox 属性
+    $(this).attr({
+      "data-src": originalSrc,  // 设置 data-src 属性
+      "src": "",                 // 清空 src 属性
+      "data-fancybox": "gallery" // 添加 data-fancybox 属性
+    });
+  });
+
+  // 初始化 LazyLoad
+  const lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy", // 选择懒加载的元素
   });
 
   // 初始化 Fancybox
